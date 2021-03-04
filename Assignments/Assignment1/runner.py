b1eb0b44d373b07eb98541af1d25ba2f1372d54d
rudradesai200 <rudradesai200@gmail.com>
2021-03-05 01:17:54 +0530

    Removed some mistakes and added some optimizations

--- Assignments/Assignment1/runner.py
+++ Assignments/Assignment1/runner.py
@@ -2,17 +2,20 @@ import models
 import optimizers
 import numpy as np
 
-X = np.random.random((1000, 10))
-t = np.sum(X ** 2, axis=1)
-y = (t > np.mean(t)) * 1
+np.random.seed(0)
+X = np.random.random((1000, 10)) - 0.5
+t = np.sum(X, axis=-1)
+y = (t > 0) * 1
+
+print(((np.count_nonzero(y)/len(y)))*100)
 
 model = models.Sequential()
 model.add(models.Dense(20, input_dim=10))
 model.add(models.Dense(2))
 model.add(models.Softmax())
 
-opt = optimizers.Nadam()
-model.compile(opt)
+opt = optimizers.SGD(1e-6, 1)
+model.compile(opt, metrics=["cross_entropy"])
 
-model.fit(X, y, epochs = 1000, verbose = 100)
+model.fit(X, y, epochs=200, verbose=10)
 model.evaluate(X, y)
