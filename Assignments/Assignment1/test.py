956c15a93d6d8bdfacd291737f9a9d6d64829a15
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-03-11 22:39:59 +0530

    Added heatmap of confusion matrix

--- Assignments/Assignment1/test.py
+++ Assignments/Assignment1/test.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
 import wandb
 import models
 import optimizers
 import numpy as np
 from keras.datasets import fashion_mnist
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import confusion_matrix
+import seaborn as sns 
+import matplotlib as plt
 # Set up your default hyperparameters before wandb.init
 # so they get properly set in the sweep
 
@@ -64,10 +72,66 @@ model.fit(train_x, train_y, epochs=config.epochs, verbose=10,
 
 y_pred = model.predict(test_x)
 
+"""
 wandb.log({"conf_mat" : wandb.plot.confusion_matrix(
                         probs=None,
                         y_true=test_y,
                         preds=y_pred,
                         class_names=class_names)})
-
+"""
 # Log metrics inside your training loop
+# ""
+
+
+# In[26]:
+
+
+test_y_name = [class_names[e] for e in test_y]
+y_pred_name = [class_names[e] for e in y_pred]
+
+def plot_cm(y_true, y_pred, figsize=(15,15)):
+    cm = confusion_matrix(y_true, y_pred, labels=np.unique(y_true))
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n' % (p)
+    cm = pd.DataFrame(cm, index=np.unique(y_true), columns=np.unique(y_true))
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(cm, cmap= "Greens", annot=annot, fmt='', ax=ax)
+    
+plot_cm(test_y_name, y_pred_name)
+
+
+# In[16]:
+
+
+test_acc = 0
+for i in range(0, len(cf_matrix)) :
+  test_acc += (cf_matrix[i][i]) / 100
+print(test_acc)
+
+
+# In[24]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
