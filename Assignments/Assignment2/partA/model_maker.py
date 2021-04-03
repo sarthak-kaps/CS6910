029634914b9c0c72397c37af59a2e795439beaa4
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-03 20:15:26 +0530

    Add dim_ordering = th, to resolve issue

--- Assignments/Assignment2/partA/model_maker.py
+++ Assignments/Assignment2/partA/model_maker.py
@@ -43,7 +43,7 @@ def make_model(input_shape, num_classes, config) :
       model.add(BatchNormalization())
     
     model.add(layers.Activation(config.convolution_activation))
-    model.add(layers.MaxPooling2D(pool_size = (config.pool_size, config.pool_size)))
+    model.add(layers.MaxPooling2D(pool_size = (config.pool_size, config.pool_size), dim_ordering="th"))
     
   if config.dropout > 0 :
     model.add(layers.Dropout(config.dropout))
