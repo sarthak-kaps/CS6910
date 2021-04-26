5299110fde6c86ac707971601f242a4ea077c8a2
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-26 19:31:22 +0530

    Improve encoder code, generalize for multiple files

--- Assignments/Assignment3/encode_input.py
+++ Assignments/Assignment3/encode_input.py
@@ -18,69 +18,115 @@ def clean_files () :
       data = pd.read_table("temp")
       data.to_csv(filename_extension + ".csv")
 
-# Vectorize the data.
-def vectorize_data(data_path) :
-    
-    input_texts = []
-    target_texts = []
-    input_characters = set()
-    target_characters = set()
-    
-    with open(data_path, "r", encoding="utf-8") as f:
-        lines = f.read().split("\n")
-    for line in lines[:len(lines) - 1]:
-        input_text, target_text, _ = line.split("\t")
-        # We use "tab" as the "start sequence" character
-        # for the targets, and "\n" as "end sequence" character.
-        target_text = "\t" + target_text + "\n"
-        input_texts.append(input_text)
-        target_texts.append(target_text)
-        for char in input_text:
-            if char not in input_characters:
-                input_characters.add(char)
-        for char in target_text:
-            if char not in target_characters:
-                target_characters.add(char)
+class one_hot_encoder :
+    # some encoder parameters
+    num_encoder_tokens = 0
+    num_decoder_tokens = 0
+    max_encoder_seq_length = 0
+    max_decoder_seq_length = 0
 
-    input_characters = sorted(list(input_characters))
-    target_characters = sorted(list(target_characters))
-    num_encoder_tokens = len(input_characters)
-    num_decoder_tokens = len(target_characters)
-    max_encoder_seq_length = max([len(txt) for txt in input_texts])
-    max_decoder_seq_length = max([len(txt) for txt in target_texts])
+    encoder_input_data = []
+    decoder_input_data = []
+    decoder_target_data = []
 
-    print("Number of samples:", len(input_texts))
-    print("Number of unique input tokens:", num_encoder_tokens)
-    print("Number of unique output tokens:", num_decoder_tokens)
-    print("Max sequence length for inputs:", max_encoder_seq_length)
-    print("Max sequence length for outputs:", max_decoder_seq_length)
+    def __init__(self, data_paths, file_labels) :
+        assert(len(data_paths) == len(file_labels))
+        self.data_paths = data_paths
+        self.file_labels = file_labels
 
-    input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
-    target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
+    # Vectorize the data.
+    # Does one hot encoding
+    def vectorize_data(self) :
+        
+        input_texts = []
+        target_texts = []
+        input_characters = set()
+        target_characters = set()
+      
+        combined_file = open("encode_data.tsv", mode = 'wt', encoding = 'utf-8')
 
-    encoder_input_data = np.zeros(
-        (len(input_texts), max_encoder_seq_length, num_encoder_tokens), dtype="float32"
-    )
-    decoder_input_data = np.zeros(
-        (len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
-    )
-    decoder_target_data = np.zeros(
-        (len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
-    )
+        file_lengths = []
 
-    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
-        for t, char in enumerate(input_text):
-            encoder_input_data[i, t, input_token_index[char]] = 1.0
-    #     encoder_input_data[i, t + 1 :, input_token_index[" "]] = 1.0
-        for t, char in enumerate(target_text):
-            # decoder_target_data is ahead of decoder_input_data by one timestep
-            decoder_input_data[i, t, target_token_index[char]] = 1.0
-            if t > 0:
-                # decoder_target_data will be ahead by one timestep
-                # and will not include the start character.
-                decoder_target_data[i, t - 1, target_token_index[char]] = 1.0
-    #     decoder_input_data[i, t + 1 :, target_token_index[" "]] = 1.0
-    #     decoder_target_data[i, t:, target_token_index[" "]] = 1.0
-    return encoder_input_data, decoder_input_data, decoder_target_data
+        for filename in self.data_paths :
+            with open(filename, mode='rt', encoding='utf-8') as ff :
+                file_lengths.append(len(ff.readlines()))
 
-vectorize_data(data_path = "dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled.train.tsv")
+            data_file = open(filename, mode='rt', encoding='utf-8')
+      
+            for line in data_file.readlines() :
+                combined_file.write(line)
+        
+        combined_file.close()
+
+        with open("encode_data.tsv", "r", encoding="utf-8") as f:
+            lines = f.read().split("\n")
+        for line in lines[:len(lines) - 1]:
+            input_text, target_text, _ = line.split("\t")
+            # We use "tab" as the "start sequence" character
+            # for the targets, and "\n" as "end sequence" character.
+            target_text = "\t" + target_text + "\n"
+            input_texts.append(input_text)
+            target_texts.append(target_text)
+            for char in input_text:
+                if char not in input_characters:
+                    input_characters.add(char)
+            for char in target_text:
+                if char not in target_characters:
+                    target_characters.add(char)
+
+        input_characters = sorted(list(input_characters))
+        target_characters = sorted(list(target_characters))
+        self.num_encoder_tokens = len(input_characters)
+        self.num_decoder_tokens = len(target_characters)
+        max_encoder_seq_length = max([len(txt) for txt in input_texts])
+        max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+        print("Number of samples:", len(input_texts))
+        print("Number of unique input tokens:", self.num_encoder_tokens)
+        print("Number of unique output tokens:", self.num_decoder_tokens)
+        print("Max sequence length for inputs:", max_encoder_seq_length)
+        print("Max sequence length for outputs:", max_decoder_seq_length)
+
+        input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
+        target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
+
+        self.encoder_input_data = np.zeros(
+            (len(input_texts), max_encoder_seq_length, self.num_encoder_tokens), dtype="float32"
+        )
+        self.decoder_input_data = np.zeros(
+            (len(input_texts), max_decoder_seq_length, self.num_decoder_tokens), dtype="float32"
+        )
+        self.decoder_target_data = np.zeros(
+            (len(input_texts), max_decoder_seq_length, self.num_decoder_tokens), dtype="float32"
+        )
+
+        for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+            for t, char in enumerate(input_text):
+                self.encoder_input_data[i, t, input_token_index[char]] = 1.0
+        #     encoder_input_data[i, t + 1 :, input_token_index[" "]] = 1.0
+            for t, char in enumerate(target_text):
+                # decoder_target_data is ahead of decoder_input_data by one timestep
+                self.decoder_input_data[i, t, target_token_index[char]] = 1.0
+                if t > 0:
+                    # decoder_target_data will be ahead by one timestep
+                    # and will not include the start character.
+                    self.decoder_target_data[i, t - 1, target_token_index[char]] = 1.0
+        #     decoder_input_data[i, t + 1 :, target_token_index[" "]] = 1.0
+        #     decoder_target_data[i, t:, target_token_index[" "]] = 1.0
+        
+        curr_length = 0
+        encoder_input_datas = {}
+        decoder_input_datas = {}
+        decoder_target_datas = {}
+
+        for (lengths, file_label) in zip(file_lengths, self.file_labels) :
+            encoder_input_datas[file_label] = self.encoder_input_data[curr_length : curr_length + lengths, :, :]
+            decoder_input_datas[file_label] = self.decoder_input_data[curr_length : curr_length + lengths, :, :]
+            decoder_target_datas[file_label] = self.decoder_target_data[curr_length : curr_length + lengths, :, :]
+            curr_length += lengths
+        return encoder_input_datas, decoder_input_datas, decoder_target_datas
+'''
+#sample usage example
+train_data_encoder = one_hot_encoder("dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled.train.tsv") 
+train_data_encoder.vectorize_data()
+'''
