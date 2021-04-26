9fe33f542426f3b990212a1d21bb669c3b21f5a3
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-26 22:56:31 +0530

    Add support for all cells

--- Assignments/Assignment3/model_maker.py
+++ Assignments/Assignment3/model_maker.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
 
-# TODO : support cells other than LSTM
 # TODO : add support for Dropout
 def make_model(config, input_shape_encoder, input_shape_decoder) :
     encoder_inputs = tf.keras.Input(shape = (None, input_shape_encoder))
@@ -11,19 +10,47 @@ def make_model(config, input_shape_encoder, input_shape_decoder) :
     for i in range(0, len(config.layer_dimensions)) :
         # if it is not the last LSTM unit we need to set return_sequence = true
         if i < len(config.layer_dimensions) - 1:
-            encoder_outputs, h, c = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True, return_sequences = True)(encoder_outputs)
+            if config.cell_type == "LSTM" :
+                encoder_outputs, h, c = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True, return_sequences = True)(encoder_outputs)
+            elif config.cell_type == "GRU" :
+                encoder_outputs, c = tf.keras.layers.GRU(config.layer_dimensions[i], return_state = True, return_sequences = True)(encoder_outputs)
+            elif config.cell_type == "RNN" :
+                encoder_outputs, c = tf.keras.layers.RNN(config.layer_dimensions[i], return_state = True, return_sequences = True)(encoder_outputs)
+            else :
+                raise ValueError("Invalid cell type {}")
         else :
-            encoder_outputs, h, c = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True)(encoder_outputs)
+            if config.cell_type == "LSTM" : 
+                encoder_outputs, h, c = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True)(encoder_outputs)
+            elif config.cell_type == "GRU" :
+                encoder_outputs, c = tf.keras.layers.GRU(config.layer_dimensions[i], return_state = True)(encoder_outputs)
+            elif config.cell_type == "RNN" :
+                encoder_outputs, c = tf.keras.layers.RNN(config.layer_dimensions[i], return_state = True)(encoder_outputs)
+            else :
+                raise ValueError("Invalid cell type {}")
 
-        encoder_states += [h, c]
+        if config.cell_type == "LSTM" :
+            encoder_states += [h, c]
+        else :
+            encoder_states += [c]
 
     decoder_inputs = tf.keras.Input(shape = (None, input_shape_decoder))
 
     decoder_outputs = decoder_inputs
     for i in range(0, len(config.layer_dimensions)) :
-        layer = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True, return_sequences = True)
+        if config.cell_type == "LSTM" :
+            layer = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True, return_sequences = True)
 
-        decoder_outputs, _, _ = layer(decoder_outputs, initial_state = encoder_states[2 * i : 2 * (i + 1)])
+            decoder_outputs, _, _ = layer(decoder_outputs, initial_state = encoder_states[2 * i : 2 * (i + 1)])
+        
+        elif config.cell_type == "GRU" :
+            layer = tf.keras.layers.GRU(config.layer_dimensions[i], return_state = True, return_sequences = True)
+
+            decoder_outputs, _ = layer(decoder_outputs, initial_state = encoder_states[i : i + 1])
+        
+        else
+            layer = tf.keras.layers.RNN(config.layer_dimensions[i], return_state = True, return_sequences = True)
+
+            decoder_outputs, _ = layer(decoder_outputs, initial_state = encoder_states[i : i + 1])
 
 
 
