0bebd9d89a173b8352801afa6566f08bfd8490df
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-26 22:39:09 +0530

    Create generic model maker

---
+++ Assignments/Assignment3/model_maker.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# TODO : support cells other than LSTM
+# TODO : add support for Dropout
+def make_model(config, input_shape_encoder, input_shape_decoder) :
+    encoder_inputs = tf.keras.Input(shape = (None, input_shape_encoder))
+
+    encoder_outputs = encoder_inputs
+    encoder_states = []
+    for i in range(0, len(config.layer_dimensions)) :
+        # if it is not the last LSTM unit we need to set return_sequence = true
+        if i < len(config.layer_dimensions) - 1:
+            encoder_outputs, h, c = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True, return_sequences = True)(encoder_outputs)
+        else :
+            encoder_outputs, h, c = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True)(encoder_outputs)
+
+        encoder_states += [h, c]
+
+    decoder_inputs = tf.keras.Input(shape = (None, input_shape_decoder))
+
+    decoder_outputs = decoder_inputs
+    for i in range(0, len(config.layer_dimensions)) :
+        layer = tf.keras.layers.LSTM(config.layer_dimensions[i], return_state = True, return_sequences = True)
+
+        decoder_outputs, _, _ = layer(decoder_outputs, initial_state = encoder_states[2 * i : 2 * (i + 1)])
+
+
+
+    decoder_dense = tf.keras.layers.Dense(input_shape_decoder, activation = 'softmax')
+
+    decoder_outputs = decoder_dense(decoder_outputs)
+
+    model = tf.keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+    return model
+
