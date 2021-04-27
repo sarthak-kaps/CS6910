7e67f34462d9b767e1e8e101176dbbc8eea9f999
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-28 00:32:45 +0530

    Clean up code

--- Assignments/Assignment3/model_maker_inference.py
+++ Assignments/Assignment3/model_maker_inference.py
@@ -9,18 +9,20 @@ from tensorflow.python.keras.backend import dropout
 def make_inference_model(model, config) :
     
     encoder_inp = model.layers[0].output
-    prev_layer_output = encoder_inp
+    prev_layer_out = encoder_inp
     encoder_states = []
     
     for i in range(0, len(config.layer_dimensions)):
-        layer_output = model.layers[2 + 2 * i](prev_layer_output)
-        prev_layer_output = layer_output[0]
+        layer_output = model.layers[2 + 2 * i](prev_layer_out)
+        prev_layer_out = layer_output[0]
         states = list(layer_output[1:])
         encoder_states.extend(list(states))
     
     encoder_model = tf.keras.Model(encoder_inp, encoder_states)
 
 
+    encoder_out = prev_layer_out
+    
     decoder_inp = model.layers[1].output
     prev_layer_out = decoder_inp
     
@@ -34,15 +36,6 @@ def make_inference_model(model, config) :
         prev_layer_out = layer_output[0]
         output_layers.append(layer)
         
-    if(config.attention):
-        attn_layer = AttentionLayer()
-        attn_out,  attn_states = attn_layer([encoder_out, prev_layer_out])
-        prev_layer_out = Concatenate(axis=-1)([prev_layer_out, attn_out])
-
-
-    prev_layer_out = model.layers[6](prev_layer_out)
-    final_output = model.layers[7](prev_layer_out)
-
     latent_dims = config.layer_dimensions
     d_outputs = decoder_inp
 
@@ -60,12 +53,13 @@ def make_inference_model(model, config) :
         decoder_states_inputs += current_state_inputs
     
 
-    d_outputs = model.layers[6](d_outputs)
+    d_outputs = model.layers[-2](d_outputs)
 
-    decoder_outputs = model.layers[7](d_outputs)
+    decoder_outputs = model.layers[-1](d_outputs)
 
     decoder_model = tf.keras.Model(
         [decoder_inp] + decoder_states_inputs,
         [decoder_outputs] + decoder_states)
 
     return encoder_model, decoder_model
+
