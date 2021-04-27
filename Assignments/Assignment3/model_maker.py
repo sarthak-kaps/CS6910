9bf79ffe3c5b5b93f274a5e1fb17eee3e73afc00
rudradesai200 <rudradesai200@gmail.com>
2021-04-27 15:03:28 +0530

    Added attention and modified some code

--- Assignments/Assignment3/model_maker.py
+++ Assignments/Assignment3/model_maker.py
@@ -1,4 +1,6 @@
-from tensorflow.keras.layers import LSTM, GRU, RNN, Dense, Concatenate, TimeDistributed, Attention
+from tensorflow.keras.layers import LSTM, GRU, RNN, Dense, Concatenate, TimeDistributed
+from tensorflow.python.keras.layers.dense_attention import Attention
+from attention import AttentionLayer
 from tensorflow.keras import Input, Model
 from tensorflow.python.keras.backend import dropout
 
@@ -12,13 +14,13 @@ def make_cell(config, **kwargs):
         raise ValueError("Invalid cell type {}", config.cell_type)
 
 
-def make_model(config, input_shape_encoder, input_shape_decoder):
+def make_model(config, enc_timesteps, enc_vsize, dec_timesteps, dec_vsize):
 
-    encoder_inp = Input(shape=(None, input_shape_encoder))
+    encoder_inp = Input(shape=(enc_timesteps, enc_vsize))
     prev_layer_out = encoder_inp
     encoder_states = []
     for i in range(0, len(config.layer_dimensions)):
-        return_seqs = (i < len(config.layer_dimensions) - 1)
+        return_seqs = True
         layer_output = make_cell(
             config, units=config.layer_dimensions[i],  return_sequences=return_seqs)(prev_layer_out)
         prev_layer_out = layer_output[0]
@@ -26,7 +28,7 @@ def make_model(config, input_shape_encoder, input_shape_decoder):
         encoder_states.extend(list(states))
 
     encoder_out = prev_layer_out
-    decoder_inp = Input(shape=(None, input_shape_decoder))
+    decoder_inp = Input(shape=(dec_timesteps, dec_vsize))
     prev_layer_out = decoder_inp
     for i in range(0, len(config.layer_dimensions)):
         # Why do we always start with factor = 1?
@@ -40,15 +42,14 @@ def make_model(config, input_shape_encoder, input_shape_decoder):
     print(encoder_out.shape, prev_layer_out.shape)
 
     if(config.attention):
-        attn_layer = Attention(
-            input_shape_encoder)
+        attn_layer = AttentionLayer()
         attn_out,  attn_states = attn_layer([encoder_out, prev_layer_out])
         prev_layer_out = Concatenate(axis=-1)([prev_layer_out, attn_out])
 
     # Why 2 adding dropout here and below too?
     # prev_layer_out = Dropout(config.dropout)(prev_layer_out)
 
-    fc_layer = Dense(input_shape_decoder, activation='softmax')
+    fc_layer = Dense(dec_vsize, activation='softmax')
     softmax_time = TimeDistributed(fc_layer)
     final_output = softmax_time(prev_layer_out)
 
@@ -58,20 +59,14 @@ def make_model(config, input_shape_encoder, input_shape_decoder):
     return model
 
 
-# class default_config:
-#     cell_type = "LSTM"
-#     layer_dimensions = [10]
-#     attention = True
-#     dropout = 0.1
-#     recurrent_dropout = 0.1
+if __name__ == "__main__":
+    class default_config:
+        cell_type = "LSTM"
+        layer_dimensions = [10]
+        attention = True
+        dropout = 0.1
+        recurrent_dropout = 0.1
 
-
-# default_config = {
-#     "cell_type": "LSTM",
-#     "layer_dimensions": [10],
-#     "attention": False,
-#     "dropout": 0.1,
-#     "recurrent_dropout": 0.1,
-# }
+    model = make_model(default_config(), 20, 30, 20, 30)
 
 # model = make_model(default_config, (100, 200), (100, 200))
