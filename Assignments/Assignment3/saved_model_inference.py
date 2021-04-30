ddcf90fb32c34136e88481fc69f7cb06c5a7671a
rudradesai200 <rudradesai200@gmail.com>
2021-04-30 14:33:51 +0530

    Updated and improved model

--- Assignments/Assignment3/saved_model_inference.py
+++ Assignments/Assignment3/saved_model_inference.py
@@ -6,29 +6,21 @@ import encode_input
 import model_maker
 import model_maker_inference
 import numpy as np
-
-# Wandb default config
-config_defaults = {
-    "epochs": 10,
-    "batch_size": 128,
-    "layer_dimensions": [128, 64],
-    "cell_type": "LSTM",
-    "dropout": 0.1,
-    "recurrent_dropout": 0.1,
-    "optimizer": "adam",
-    "attention": False,
-}
-
-# Initialize the project
-wandb.init(project='assignment3',
-           group='First Run',
-           config=config_defaults)
-
-# config file used for the current run
-config = wandb.config
+from tqdm import tqdm
 
 
-wandb.run.name = f"cell_type_{config.cell_type}_layer_org_{"".join(config.layer_dimensions)}_drpout_{int(config.dropout*100)}%_rec-drpout_{int(config.recurrent_dropout*100)}%_bs_{config.batch_size}_opt_{config.optimizer}"
+class default_config:
+    cell_type = "LSTM"
+    layer_dimensions = [128]
+    attention = False
+    dropout = 0.1
+    recurrent_dropout = 0.1
+    optimizer = "adam"
+    batch_size = None
+    epochs = 10
+
+
+config = default_config()
 
 
 base_data_set_name = "dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled."
@@ -36,39 +28,12 @@ base_data_set_name = "dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled."
 data_encoder = encode_input.one_hot_encoder(
     [base_data_set_name + "train.tsv", base_data_set_name + "dev.tsv", base_data_set_name + "test.tsv"], ["train", "valid", "test"])
 
-
-# encoder_input_data, decoder_input_data, decoder_target_data are dictionaries and contain the encoder input
-# decoder input and the target data all in one hot encoding format for the train, valid and test datasets.
-# Example -
-# To obtain the encoder input for train data do encoder_input_data["train"]
-# To obtain the decoder input for validation data do decoder_input_data["valid"]
 [encoder_input_data, decoder_input_data,
     decoder_target_data, input_texts_dict, target_texts_dict] = data_encoder.vectorize_data()
 
-# construct the model from the given configurations
-model = model_maker.make_model(
-    config, data_encoder.max_encoder_seq_length, data_encoder.num_encoder_tokens, data_encoder.max_decoder_seq_length, data_encoder.num_decoder_tokens)
 
-print(model.summary())
+model = keras.models.load_model("model1")
 
-# compile the model
-model.compile(
-    optimizer=config.optimizer, loss="categorical_crossentropy", metrics=["accuracy"]
-)
-
-# fit the model
-model.fit(
-    x=[encoder_input_data["train"], decoder_input_data["train"]],
-    y=decoder_target_data["train"],
-    batch_size=config.batch_size,
-    epochs=config.epochs,
-    # validation_data=([encoder_input_data["valid"],
-    #                   decoder_input_data["valid"]], decoder_target_data["valid"]),
-    callbacks=[WandbCallback()]
-)
-
-# Save model
-model.save(wandb.run.name)
 
 encoder_model, decoder_model = model_maker_inference.make_inference_model(
     model, config)
@@ -118,16 +83,24 @@ def decode_sequence(input_seq, encoder_model, decoder_model):
     return "".join(decoded_sentence)
 
 
-for seq_index in range(20):
+accuracy = 0
+
+for seq_index in tqdm(range(len(encoder_input_data["valid"]))):
     # Take one sequence (part of the training set)
     # for trying out decoding.
     input_seq = encoder_input_data["valid"][seq_index: seq_index + 1]
     decoded_sentence = decode_sequence(
         input_seq, encoder_model, decoder_model)[:-1]
     target_sentence = target_texts_dict["valid"][seq_index: seq_index + 1][0][1:-1]
-    print("-")
-    print("Input sentence:",
-          input_texts_dict["valid"][seq_index: seq_index + 1])
-    print("Decodd sentence:", decoded_sentence)
-    print("Target sentence:",
-          target_sentence)
+    if(decoded_sentence == target_sentence):
+        accuracy += 1
+
+    if(seq_index < 20):
+        print("-")
+        print("Input sentence:",
+              input_texts_dict["valid"][seq_index: seq_index + 1])
+        print("Decodd sentence:", decoded_sentence)
+        print("Target sentence:", target_sentence)
+
+accuracy /= len(encoder_input_data["valid"])
+print("Accuracy : ", accuracy)
