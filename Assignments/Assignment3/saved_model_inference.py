8f168db8401a934877f83e64d3edc1cce79a0413
rudradesai200 <rudradesai200@gmail.com>
2021-05-02 05:01:25 +0530

    Added attention layer and updated inference model

--- Assignments/Assignment3/saved_model_inference.py
+++ Assignments/Assignment3/saved_model_inference.py
@@ -1,26 +1,9 @@
-import tensorflow as tf
-from tensorflow import keras
-import wandb
-from wandb.keras import WandbCallback
-import encode_input
-import model_maker
-import model_maker_inference
 import numpy as np
+from tensorflow import keras
 from tqdm import tqdm
 
-
-class default_config:
-    cell_type = "LSTM"
-    layer_dimensions = [128, 64]
-    attention = False
-    dropout = 0.1
-    recurrent_dropout = 0.1
-    optimizer = "adam"
-    batch_size = 128
-    epochs = 10
-
-
-config = default_config()
+import encode_input
+import wandb
 
 
 base_data_set_name = "dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled."
@@ -32,11 +15,10 @@ data_encoder = encode_input.one_hot_encoder(
     decoder_target_data, input_texts_dict, target_texts_dict] = data_encoder.vectorize_data()
 
 
-model = keras.models.load_model("model2")
+model = keras.models.load_model("model2/train")
+inf_enc_model = keras.models.load_model("model2/inf-enc")
+inf_dec_model = keras.models.load_model("model2/inf-dec")
 
-
-encoder_model, decoder_model = model_maker_inference.make_inference_model(
-    model, config)
 
 # Reverse-lookup token index to decode sequences back to
 # something readable.
@@ -48,7 +30,7 @@ reverse_target_char_index = dict(
 
 def decode_sequence(input_seq, encoder_model, decoder_model):
     # Encode the input as state vectors.
-    states_value = encoder_model.predict(input_seq)
+    states_value, enc_out = encoder_model.predict(input_seq)
 
     # Generate empty target sequence of length 1.
     target_seq = np.zeros((1, 1, data_encoder.num_decoder_tokens))
@@ -61,9 +43,10 @@ def decode_sequence(input_seq, encoder_model, decoder_model):
     # Creating a list then using "".join() is usually much faster for string creation
     decoded_sentence = []
     while not stop_condition:
-        to_split = decoder_model.predict([target_seq] + states_value)
-
-        output_tokens, states_value = to_split[0], to_split[1:]
+        to_split = decoder_model.predict(
+            [target_seq, states_value, enc_out])
+        output_tokens, states_value, attn_weights = to_split[0], list(
+            to_split[1:-1]), to_split[-1]
 
         # Sample a token
 #         print(output_tokens)
@@ -83,26 +66,57 @@ def decode_sequence(input_seq, encoder_model, decoder_model):
     return "".join(decoded_sentence)
 
 
-accuracy = 0
+def editDistance(str1, str2, m, n):
+    # Create a table to store results of subproblems
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
+
+    # Fill d[][] in bottom up manner
+    for i in range(m + 1):
+        for j in range(n + 1):
+
+            # If first string is empty, only option is to
+            # insert all characters of second string
+            if i == 0:
+                dp[i][j] = j    # Min. operations = j
+
+            # If second string is empty, only option is to
+            # remove all characters of second string
+            elif j == 0:
+                dp[i][j] = i    # Min. operations = i
+
+            # If last characters are same, ignore last char
+            # and recur for remaining string
+            elif str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+
+            # If last character are different, consider all
+            # possibilities and find minimum
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1],        # Insert
+                                   dp[i-1][j],        # Remove
+                                   dp[i-1][j-1])    # Replace
+
+    return dp[m][n]
+
+
 input_seqs = encoder_input_data["valid"]
 target_sents = target_texts_dict["valid"]
-for seq_index in tqdm(range(len(encoder_input_data["valid"]))):
+n = len(input_seqs)
+val_avg_edit_dist = 0
+for seq_index in tqdm(range(500)):
     # Take one sequence (part of the training set)
     # for trying out decoding.
-    input_seq = input_seqs[seq_index]
-    decoded_sentence = decode_sequence(
-        input_seq, encoder_model, decoder_model)[:-1]
-    target_sentence = target_sents[seq_index][0][1:-1]
-
-    if(decoded_sentence == target_sentence):
-        accuracy += 1
-
+    input_seq = input_seqs[seq_index:seq_index+1]
+    decoded_sentence = str(decode_sequence(
+        input_seq, inf_enc_model, inf_dec_model)[:-1])
+    target_sentence = str(target_sents[seq_index:seq_index+1][0][1:-1])
+    edit_dist = editDistance(decoded_sentence, target_sentence, len(
+        decoded_sentence), len(target_sentence))/len(target_sentence)
+    val_avg_edit_dist += edit_dist
     if(seq_index < 20):
-        print("-")
-        print("Input sentence:",
-              input_texts_dict["valid"][seq_index: seq_index + 1])
-        print("Decodd sentence:", decoded_sentence)
-        print("Target sentence:", target_sentence)
+        wandb.log({f"input_{seq_index}": input_seq, f"output_{seq_index}": decoded_sentence,
+                   f"target_{seq_index}": target_sentence, f"edit_distance_{seq_index}": edit_dist})
 
-accuracy /= len(encoder_input_data["valid"])
-print("Accuracy : ", accuracy)
+val_avg_edit_dist /= 500
+
+wandb.log({"val_avg_edit_dist": val_avg_edit_dist})
