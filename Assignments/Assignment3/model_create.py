967de83912d809acf0ed704fc924a0dc9b9df2ae
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-05-03 19:15:14 +0530

    Add config file support

--- Assignments/Assignment3/model_create.py
+++ Assignments/Assignment3/model_create.py
@@ -1,21 +1,36 @@
 import tensorflow as tf
+    
+dispatch_dict = {"LSTM": tf.keras.layers.LSTM, "GRU": tf.keras.layers.GRU, "RNN": tf.keras.layers.SimpleRNN}
 
 
 class Encoder(tf.keras.Model):
-    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+    def __init__(self, vocab_size, config):
         super(Encoder, self).__init__()
-        self.batch_sz = batch_sz
-        self.enc_units = enc_units
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.gru = tf.keras.layers.GRU(self.enc_units,
+        self.config = config
+        self.batch_sz = self.config.batch_sz
+        self.layer_dimensions = self.config.layer_dimensions
+        self.embedding = tf.keras.layers.Embedding(vocab_size, self.config.embedding_dim)
+       
+        self.model = []
+        
+        for i in range(0, len(self.layer_dimensions)) :
+            self.model.append(dispatch_dict[config.cell_type](self.layer_dimensions[i],
                                        return_sequences=True,
                                        return_state=True,
-                                       recurrent_initializer='glorot_uniform')
+                                       recurrent_initializer='glorot_uniform'))
+
 
     def call(self, x, hidden):
         x = self.embedding(x)
-        output, state = self.gru(x, initial_state=hidden)
-        return output, state
+        states = []
+        for i in range(0, len(self.model)) :
+            layer_out = self.model[i](x)
+            x, state = layer_out[0], layer_out[1:]
+            state, x = concat(
+            list(layer_out[1:]), axis = -1), layer_out[0]
+            states.extend(state)
+        output = x
+        return output, states
 
     def initialize_hidden_state(self):
         return tf.zeros((self.batch_sz, self.enc_units))
@@ -23,12 +38,14 @@ class Encoder(tf.keras.Model):
 
 class BahdanauAttention(tf.keras.layers.Layer):
     def __init__(self, units):
+
         super(BahdanauAttention, self).__init__()
         self.W1 = tf.keras.layers.Dense(units)
         self.W2 = tf.keras.layers.Dense(units)
         self.V = tf.keras.layers.Dense(1)
-
+    
     def call(self, query, values):
+        # query hidden state shape == (batch_size, hidden size)
         # query hidden state shape == (batch_size, hidden size)
         # query_with_time_axis shape == (batch_size, 1, hidden size)
         # values shape == (batch_size, max_len, hidden size)
@@ -52,19 +69,24 @@ class BahdanauAttention(tf.keras.layers.Layer):
 
 
 class Decoder(tf.keras.Model):
-    def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+    def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz, config):
         super(Decoder, self).__init__()
-        self.batch_sz = batch_sz
-        self.dec_units = dec_units
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-        self.gru = tf.keras.layers.GRU(self.dec_units,
+        self.config = config
+        self.batch_sz = self.config.batch_sz
+        self.layer_dimensions = self.config.layer_dimensions
+        self.embedding = tf.keras.layers.Embedding(vocab_size, self.config.embedding_dim)
+
+        self.model = []
+        for i in range(0, len(self.layer_dimensions)) :
+            self.model.append(dispatch_dict[config.cell_type](self.layer_dimensions[i],
                                        return_sequences=True,
                                        return_state=True,
-                                       recurrent_initializer='glorot_uniform')
+                                       recurrent_initializer='glorot_uniform'))
+
         self.fc = tf.keras.layers.Dense(vocab_size)
 
         # used for attention
-        self.attention = BahdanauAttention(self.dec_units)
+        self.attention = BahdanauAttention(self.config.attention_shape)
 
     def call(self, x, hidden, enc_output):
         # enc_output shape == (batch_size, max_length, hidden_size)
@@ -76,13 +98,19 @@ class Decoder(tf.keras.Model):
         # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
 
-        # passing the concatenated vector to the GRU
-        output, state = self.gru(x)
-
+        # passing the concatenated vector through the model
+        states = []
+        for i in range(0, len(self.model)) :
+            layer_out = self.model[i](x)
+            state, x = concat(
+            list(layer_out[1:]), axis=-1), layer_out[0]
+            states.extend(state)
+        
+        output = x
         # output shape == (batch_size * 1, hidden_size)
         output = tf.reshape(output, (-1, output.shape[2]))
 
         # output shape == (batch_size, vocab)
         x = self.fc(output)
 
-        return x, state, attention_weights
+        return x, states, attention_weights
