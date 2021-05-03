7d15007172c8bf769b79884cb1dacb6ed1f13365
rudradesai200 <rudradesai200@gmail.com>
2021-05-03 22:41:11 +0530

    Organized code structure

---
+++ Assignments/Assignment3/codebase1/encode_input.py
@@ -0,0 +1,157 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+
+np.random.seed(1337)
+
+# convert files into csv for better readability
+
+
+def clean_files():
+    base_file_name = "dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled."
+    for filename_extension in ["train", "test", "dev"]:
+        f = open("temp", mode='wt', encoding='utf-8')
+        f.write("Hi\tEn\tLexicons\n")
+
+        filename = base_file_name + filename_extension + ".tsv"
+        data_file = open(filename, mode='rt', encoding='utf-8')
+        for line in data_file.readlines():
+            f.write(line)
+
+        data = pd.read_table("temp")
+        data.to_csv(filename_extension + ".csv")
+
+
+class one_hot_encoder:
+    # some encoder parameters
+    num_encoder_tokens = 0
+    num_decoder_tokens = 0
+    max_encoder_seq_length = 0
+    max_decoder_seq_length = 0
+
+    encoder_input_data = []
+    decoder_input_data = []
+    decoder_target_data = []
+
+    def __init__(self, data_paths, file_labels):
+        assert(len(data_paths) == len(file_labels))
+        self.data_paths = data_paths
+        self.file_labels = file_labels
+
+    # Vectorize the data.
+    # Does one hot encoding
+
+    def vectorize_data(self):
+
+        input_texts = []
+        target_texts = []
+        input_characters = set()
+        target_characters = set()
+
+        combined_file = open("encode_data.tsv", mode='wt', encoding='utf-8')
+
+        file_lengths = []
+
+        for filename in self.data_paths:
+            with open(filename, mode='rt', encoding='utf-8') as ff:
+                lines = ff.readlines()
+                file_lengths.append(len(lines))
+                for line in lines:
+                    combined_file.write(line)
+
+        combined_file.close()
+
+        with open("encode_data.tsv", "r", encoding="utf-8") as f:
+            lines = f.read().split("\n")
+
+            for line in lines[:len(lines) - 1]:
+                target_text, input_text, _ = line.split("\t")
+                # We use "tab" as the "start sequence" character
+                # for the targets, and "\n" as "end sequence" character.
+                target_text = "\t" + target_text + "\n"
+                input_texts.append(input_text)
+                target_texts.append(target_text)
+                for char in input_text:
+                    if char not in input_characters:
+                        input_characters.add(char)
+                for char in target_text:
+                    if char not in target_characters:
+                        target_characters.add(char)
+
+        input_characters = sorted(list(input_characters)) + ['\n']
+        target_characters = sorted(list(target_characters)) + ['\n']
+        self.num_encoder_tokens = len(input_characters)
+        self.num_decoder_tokens = len(target_characters)
+        self.max_encoder_seq_length = max([len(txt) for txt in input_texts])
+        self.max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+        print("Number of samples:", len(input_texts))
+        print("Number of unique input tokens:", self.num_encoder_tokens)
+        print("Number of unique output tokens:", self.num_decoder_tokens)
+        print("Max sequence length for inputs:", self.max_encoder_seq_length)
+        print("Max sequence length for outputs:", self.max_decoder_seq_length)
+
+        # Adjusting timesteps for better attention
+        # self.max_decoder_seq_length = self.max_encoder_seq_length = max(
+        #     self.max_encoder_seq_length, self.max_decoder_seq_length)
+        input_token_index = dict([(char, i)
+                                  for i, char in enumerate(input_characters)])
+        target_token_index = dict([(char, i)
+                                   for i, char in enumerate(target_characters)])
+
+        self.encoder_input_data = np.zeros(
+            (len(input_texts), self.max_encoder_seq_length, self.num_encoder_tokens), dtype="float32"
+        )
+        self.decoder_input_data = np.zeros(
+            (len(input_texts), self.max_decoder_seq_length, self.num_decoder_tokens), dtype="float32"
+        )
+        self.decoder_target_data = np.zeros(
+            (len(input_texts), self.max_decoder_seq_length, self.num_decoder_tokens), dtype="float32"
+        )
+
+        for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+            for t, char in enumerate(input_text):
+                self.encoder_input_data[i, t, input_token_index[char]] = 1.0
+                # I think this line will increase error as we are passing the whole word into RNN
+                # But, after the \n char, everything will be 0.
+                # I think we should make \n default after the word end
+            self.encoder_input_data[i, t + 1:, input_token_index["\n"]] = 1.0
+            for t, char in enumerate(target_text):
+                # decoder_target_data is ahead of decoder_input_data by one timestep
+                self.decoder_input_data[i, t, target_token_index[char]] = 1.0
+                if t > 0:
+                    # decoder_target_data will be ahead by one timestep
+                    # and will not include the start character.
+                    self.decoder_target_data[i, t - 1,
+                                             target_token_index[char]] = 1.0
+            self.decoder_input_data[i, t + 1:, target_token_index["\n"]] = 1.0
+            self.decoder_target_data[i, t:, target_token_index["\n"]] = 1.0
+
+        curr_length = 0
+        encoder_input_datas = {}
+        decoder_input_datas = {}
+        decoder_target_datas = {}
+        input_texts_dict = {}
+        target_texts_dict = {}
+
+        for (lengths, file_label) in zip(file_lengths, self.file_labels):
+            shuffler = np.random.permutation(lengths)
+            encoder_input_datas[file_label] = self.encoder_input_data[curr_length: curr_length +
+                                                                      lengths, :, :][shuffler]
+            decoder_input_datas[file_label] = self.decoder_input_data[curr_length: curr_length +
+                                                                      lengths, :, :][shuffler]
+            decoder_target_datas[file_label] = self.decoder_target_data[curr_length: curr_length +
+                                                                        lengths, :, :][shuffler]
+            input_texts_dict[file_label] = np.array(input_texts[curr_length: curr_length +
+                                                                lengths])[shuffler]
+            target_texts_dict[file_label] = np.array(target_texts[curr_length: curr_length +
+                                                                  lengths])[shuffler]
+            curr_length += lengths
+
+        # for ease of access later
+
+        self.input_token_index = input_token_index
+        self.target_token_index = target_token_index
+
+        return encoder_input_datas, decoder_input_datas, decoder_target_datas, input_texts_dict, target_texts_dict
---
+++ Assignments/Assignment3/codebase1/saved_model_inference.py
@@ -0,0 +1,150 @@
+import numpy as np
+from tensorflow import keras
+from tqdm import tqdm
+from tensorflow import reshape
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+import encode_input
+import wandb
+
+val_samples = 400
+base_data_set_name = "dakshina_dataset_v1.0/hi/lexicons/hi.translit.sampled."
+
+data_encoder = encode_input.one_hot_encoder(
+    [base_data_set_name + "train.tsv", base_data_set_name + "dev.tsv", base_data_set_name + "test.tsv"], ["train", "valid", "test"])
+
+[encoder_input_data, decoder_input_data,
+    decoder_target_data, input_texts_dict, target_texts_dict] = data_encoder.vectorize_data()
+
+
+model = keras.models.load_model("model_1/train")
+inf_enc_model = keras.models.load_model("model_1/inf-enc")
+inf_dec_model = keras.models.load_model("model_1/inf-dec")
+
+
+# Reverse-lookup token index to decode sequences back to
+# something readable.
+reverse_input_char_index = dict(
+    (i, char) for char, i in data_encoder.input_token_index.items())
+reverse_target_char_index = dict(
+    (i, char) for char, i in data_encoder.target_token_index.items())
+
+
+def decode_sequence(input_seq, encoder_model, decoder_model):
+    attention_plot = np.zeros(
+        (data_encoder.max_decoder_seq_length, data_encoder.max_encoder_seq_length))
+    # Encode the input as state vectors.
+    states_value, enc_out = encoder_model.predict(input_seq)
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, data_encoder.num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, data_encoder.target_token_index['\t']] = 1.
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    # Creating a list then using "".join() is usually much faster for string creation
+    decoded_sentence = []
+    i = 0
+    while not stop_condition:
+        to_split = decoder_model.predict(
+            [target_seq, states_value, enc_out])
+        output_tokens, states_value, attn_weights = to_split[0], list(
+            to_split[1:-1]), to_split[-1]
+        attention_weights = reshape(attn_weights, (-1, ))
+        attention_plot[i] = attention_weights.numpy()
+        # Sample a token
+#         print(output_tokens)
+        sampled_token_index = np.argmax(output_tokens[0, 0])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence.append(sampled_char)
+        # Exit condition: either hit max length
+        # or find stop character.
+        if sampled_char == '\n' or len(decoded_sentence) > data_encoder.max_decoder_seq_length:
+            stop_condition = True
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, data_encoder.num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.
+        i += 1
+    attention_plot = attention_plot[:len(decoded_sentence)]
+    return "".join(decoded_sentence), attention_plot
+
+
+def editDistance(str1, str2, m, n):
+    # Create a table to store results of subproblems
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
+
+    # Fill d[][] in bottom up manner
+    for i in range(m + 1):
+        for j in range(n + 1):
+
+            # If first string is empty, only option is to
+            # insert all characters of second string
+            if i == 0:
+                dp[i][j] = j    # Min. operations = j
+
+            # If second string is empty, only option is to
+            # remove all characters of second string
+            elif j == 0:
+                dp[i][j] = i    # Min. operations = i
+
+            # If last characters are same, ignore last char
+            # and recur for remaining string
+            elif str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+
+            # If last character are different, consider all
+            # possibilities and find minimum
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1],        # Insert
+                                   dp[i-1][j],        # Remove
+                                   dp[i-1][j-1])    # Replace
+
+    return dp[m][n]
+
+# function for plotting the attention weights
+
+
+def plot_attention(attention, sentence, predicted_sentence):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(1, 1, 1)
+    # attention = attention[:len(predicted_sentence), :len(sentence)]
+    ax.matshow(attention, cmap='viridis')
+    fontdict = {'fontsize': 14}
+    ax.set_xticklabels([''] + list(sentence), fontdict=fontdict, rotation=90)
+    ax.set_yticklabels([''] + list(predicted_sentence), fontdict=fontdict)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    plt.show()
+
+
+input_seqs = encoder_input_data["valid"]
+target_sents = target_texts_dict["valid"]
+input_sents = input_texts_dict["valid"]
+n = len(input_seqs)
+val_avg_edit_dist = 0
+acc = 0
+for seq_index in range(val_samples):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = input_seqs[seq_index:seq_index+1]
+    decoded_str, attention_plot = decode_sequence(
+        input_seq, inf_enc_model, inf_dec_model)
+    decoded_sentence = str(decoded_str[:-1])
+    target_sentence = str(target_sents[seq_index:seq_index+1][0][1:-1])
+    edit_dist = editDistance(decoded_sentence, target_sentence, len(
+        decoded_sentence), len(target_sentence))/len(target_sentence)
+    val_avg_edit_dist += edit_dist
+    if(decoded_sentence == target_sentence):
+        acc += 1
+    if(seq_index < 0):
+        plot_attention(
+            attention_plot, input_sents[seq_index], decoded_sentence)
+    # if(seq_index < 20):
+    print({f"output_{seq_index}": decoded_sentence,
+           f"target_{seq_index}": target_sentence, f"edit_distance_{seq_index}": edit_dist})
+
+val_avg_edit_dist /= val_samples
+acc /= val_samples
+
+print({"val_avg_edit_dist": val_avg_edit_dist, "val_acc": acc})
