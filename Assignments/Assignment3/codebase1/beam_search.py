5961ad38eb812b3f2fa48ca173a5d2700855320e
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-05-04 17:12:03 +0530

    Imporve beam search

--- Assignments/Assignment3/codebase1/beam_search.py
+++ Assignments/Assignment3/codebase1/beam_search.py
@@ -11,6 +11,12 @@ class Sequence :
     def add(self, character, probability) :
         self.characters.append(character)
         self.sum_log_probability += tf.math.log(probability)
+        
+    def set_state(self, state) :
+      self.state = state
+    
+    def get_state(self) :
+      return self.state
 
     def get_top(self) :
         return self.characters[-1]
@@ -34,7 +40,8 @@ class BeamSearch :
         self.eos = eos
         
     def apply(self, encoder, decoder, input_seq) :
-        states_values = [encoder(input_seq)] * self.beam_width
+        states_value, enc_out = encoder.predict(input_seq)
+        self.best_seq_live[0].set_state(states_value)
         final_seq_list = []
         
         for _ in range(0, self.max_length) :
@@ -48,13 +55,12 @@ class BeamSearch :
                 curr_character = seq.get_top()
             
                 target_seq[0, 0, self.decoder_character_map[curr_character]] = 1
-
                 
-                if type(states_values[i]) != list:
-                    to_split = decoder.predict([target_seq] + [states_values[i]])
-                else:
-                    to_split = decoder.predict([target_seq] + states_values[i])
-                output_tokens, states_values[i] = to_split[0], to_split[1:]
+                to_split = decoder.predict([target_seq, seq.get_state(), enc_out])
+
+                output_tokens, new_states_values, attn_weights = to_split[0], list(
+                to_split[1:-1]), to_split[-1]
+
 
                 top_probabilities, indexes = tf.nn.top_k(output_tokens[0, 0], self.beam_width)
 #                 print(top_probabilities)
@@ -64,20 +70,21 @@ class BeamSearch :
                     new_seq = copy.deepcopy(seq)
 #                     print(self.reverse_decoder_char_map[index.numpy()])
                     new_seq.add(self.reverse_decoder_char_map[index.numpy()], top_probabilities[i].numpy())
+                    new_seq.set_state(new_states_values)
                     if new_seq.get_top() == self.eos :
                         self.best_seq_dead.append(new_seq)
                     else :
                         new_best_seq_live.append(new_seq)
 
             self.best_seq_live = []
-            new_best_seq_live = sorted(new_best_seq_live, key = lambda seq1 : -seq1.sum_log_probability.numpy() / (len(seq1.characters) ** (0.7)))
+            new_best_seq_live = sorted(new_best_seq_live, key = lambda seq1 : -seq1.sum_log_probability.numpy() / (len(seq1.characters) ** 0.7))
             
             self.best_seq_live = new_best_seq_live[:self.beam_width]
 #             print(list(map(lambda x : x.characters, self.best_seq_live)))
 
         
         final_seq_list = self.best_seq_dead + self.best_seq_live
-        final_seq_list = sorted(final_seq_list, key = lambda seq1 : -seq1.sum_log_probability.numpy() / (len(seq1.characters) ** (0.7)))
+        final_seq_list = sorted(final_seq_list, key = lambda seq1 : -seq1.sum_log_probability.numpy() / (len(seq1.characters) ** 0.7))
     
 #         print(list(map(lambda x : x.characters, final_seq_list))) 
       
